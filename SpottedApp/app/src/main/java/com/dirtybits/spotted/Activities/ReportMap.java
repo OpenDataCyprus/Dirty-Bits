943539cb73110c75b671cbdb6f4602852eed9510
George Kapetanios <gkapetanios13@gmail.com>
2016-09-10 22:27:58 +0300

    Report Map

--- SpottedApp/app/src/main/java/com/dirtybits/spotted/Activities/ReportMap.java
+++ SpottedApp/app/src/main/java/com/dirtybits/spotted/Activities/ReportMap.java
@@ -27,6 +27,6 @@ public class ReportMap extends FragmentActivity implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        mMap = googleMap;
     }
 }
