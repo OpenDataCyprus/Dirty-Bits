8f170dfc4cccffef8a55c0cf925147ff26c294ef
George Kapetanios <gkapetanios13@gmail.com>
2016-09-10 20:48:38 +0300

    Report and Sightings Maps

--- SpottedApp/app/src/main/java/com/dirtybits/spotted/Activities/MissingPersonInfo.java
+++ SpottedApp/app/src/main/java/com/dirtybits/spotted/Activities/MissingPersonInfo.java
@@ -1,8 +1,10 @@
 package com.dirtybits.spotted.Activities;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.dirtybits.spotted.Models.MissingPerson;
 import com.dirtybits.spotted.Models.Person;
@@ -20,5 +22,11 @@ public class MissingPersonInfo extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_missing_person_info);
         binding.setMissingPerson(_missingPerson);
 
+    }
+
+    public void showSightingsMap(View view)
+    {
+        Intent intent = new Intent(this, SightingsMaps.class);
+        startActivity(intent);
     }
 }
